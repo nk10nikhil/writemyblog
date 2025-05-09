b5d48e28d4cbc0d06cf2dc306c692eaff50f8c4d
nk10nikhil <nk10nikhil@gmail.com>
2025-05-09 17:49:43 +0530

    fix deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", {
+        "varsIgnorePattern": "User", // Ignore unused User imports specifically
+        "argsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }]
+    }
+  }
 ];
 
 export default eslintConfig;
